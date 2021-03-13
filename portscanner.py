228dae8b32cdf5d154b61a3ad6489f798593b569
Diego G <diego.gomes87@gmail.com>
2021-03-13 15:57:18 -0300

    melhorando a pesquisa

--- portscanner.py
+++ portscanner.py
@@ -2,13 +2,16 @@ import socket
 from IPy import IP
 
 
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        print('[+] Porta '+str(port)+' Abertas!')
+    except:
+        print('[-] Portas '+ str(port)+' Fechadas! ')
+
 ipaddress = input('[+] Digite o Ip do Alvo: ')
-port = 80
 
-
-try:
-    sock = socket.socket()
-    sock.connect((ipaddress, port))
-    print('[+] Portas Abertas!')
-except:
-    print('[-] Portas Fechadas! ')
+for port in range(1,10):
+    scan_port(ipaddress, port)

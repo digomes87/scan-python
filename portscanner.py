6b9b24a85927a146960d8dc21c68ccdace97c3aa
Diego G <diego.gomes87@gmail.com>
2021-03-13 15:35:19 -0300

    teste simples, buscando portas abertas apartir de um ip

--- portscanner.py
+++ portscanner.py
@@ -1,2 +1,14 @@
 import socket
 from IPy import IP
+
+
+ipaddress = input('[+] Digite o Ip do Alvo: ')
+port = 80
+
+
+try:
+    sock = socket.socket()
+    sock.connect((ipaddress, port))
+    print('[+] Portas Abertas!')
+except:
+    print('[-] Portas Fechadas! ')
